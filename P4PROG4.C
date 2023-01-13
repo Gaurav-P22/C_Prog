466588e011da9f45b5e9c557ed1aefe28b007c99
Gaurav Prajapati <118986007+GauravKiet@users.noreply.github.com>
2023-01-13 19:02:58 +0530

    Add files via upload

---
+++ P4PROG4.C
@@ -0,0 +1,46 @@
+//program to sum of the even and odd number until the number n
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+
+void EvenNumbers(int *);
+void OddNumber(int *);
+clrscr();
+printf("Enter the value of n");
+scanf("%d",&n);
+
+EvenNumbers(&n);
+OddNumber(&n);
+getch();
+
+
+}
+void EvenNumbers(int *p)
+{
+int i,sum=0;
+for(i=1;i<=*p;i++)
+{
+if(i%2==0)
+{
+
+sum=sum+i;
+}
+}
+printf("\nThe sum of Even Number is=%d",sum);
+}
+void OddNumber(int *p)
+{
+int i,sum=0;
+for(i=1;i<=*p;i++)
+{
+if(i%2==1)
+{
+sum=sum+i;
+}
+}
+printf("\nThe sum of odd Number is=%d",sum);
+
+
+}
