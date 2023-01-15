e8f3bb28eb4b3da77b233f25d304f979351e9b66
Gaurav Prajapati <118986007+GauravKiet@users.noreply.github.com>
2023-01-15 22:08:10 +0530

    Add files via upload

--- P4PROG10.C
+++ P4PROG10.C
@@ -5,5 +5,12 @@ void main()
 int i,j;
 printf("Enter the row number until u want to print the pattern\n");
 scanf("%d",&n);
-for()
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+printf(“%d”,j);
+}
+}
+getch();
 }
