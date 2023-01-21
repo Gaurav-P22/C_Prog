9c0ba192214cd9799963529e5c2d4ae3d6f6e34b
Gaurav Prajapati <118986007+GauravKiet@users.noreply.github.com>
2023-01-21 22:25:40 +0530

    Add files via upload

---
+++ P9PROG1.C
@@ -0,0 +1,19 @@
+//Program to create, initialize and use pointer
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+void Sum(int *,int *);
+int a,b;
+printf("Enter the Two value to add them");
+scanf("%d%d",&a,&b);
+Sum(&a,&b);
+getch();
+
+}
+void Sum(int *ptr1,int *ptr2)//Creating and Initializing the pointer
+{
+int c;
+c=*ptr1+(*ptr2);
+printf("The Sum of the two Numbers => %d",c);
+}
