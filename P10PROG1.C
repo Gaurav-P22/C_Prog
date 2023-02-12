f1ab2d398ace8dd59c57145d67c25f1dd0a1caf8
Gaurav Prajapati <118986007+GauravKiet@users.noreply.github.com>
2023-02-12 22:24:31 +0530

    Add files via upload

---
+++ P10PROG1.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+auto int x=4,y=5;
+void f1()
+ {
+ int result;
+
+ result=x+y;
+ printf("%d",result);
+ }
+// printf("%d",result);//trying to use f1's local variable
+
+}
