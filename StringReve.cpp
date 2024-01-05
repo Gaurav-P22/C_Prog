b8758106cb82d5066df30715bc728a6557f8da65
Gaurav Prajapati <118986007+Gaurav-P22@users.noreply.github.com>
2024-01-05 21:23:57 +0530

    Update StringReve.cpp

--- StringReve.cpp
+++ StringReve.cpp
@@ -17,3 +17,32 @@ int main() {
 
     return 0;
 }
+
+
+
+///Second Method
+
+
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+void ReverseSt(string &str,int i,int j)
+{
+    if(i>j)
+        return ;
+    swap(str[i],str[j]);
+    i++;
+    j--;
+    ReverseSt(str,i,j);
+    
+
+}
+
+int main(){
+    string str="String";
+    ReverseSt(str,0,str.length()-1);
+    cout<<str<<endl;
+    return 0;
+}
